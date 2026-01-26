3f97ff7f5265a768e354abd6c55dc32180351e0e
Shaurya <shauryasofficial27@gmail.com>
2026-01-26 11:13:41 +0530

    Update First_Project/src/First_Lesson/Temperature_Example.java

--- First_Project/src/First_Lesson/Temperature_Example.java
+++ First_Project/src/First_Lesson/Temperature_Example.java
@@ -1,4 +1,13 @@
 package First_Lesson;
 
+import java.util.Scanner;
+
 public class Temperature_Example {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your temperature in C: ");
+        float tempC = input.nextFloat();
+        float tempF = (tempC * 1.8f) + 32;
+        System.out.println(tempF);
+    }
 }
