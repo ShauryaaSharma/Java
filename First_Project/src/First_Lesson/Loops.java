453a79cb9eed231343dcbc8727a801b1576230d8
Shaurya <shauryasofficial27@gmail.com>
2026-01-26 11:13:40 +0530

    Update First_Project/src/First_Lesson/Loops.java

--- First_Project/src/First_Lesson/Loops.java
+++ First_Project/src/First_Lesson/Loops.java
@@ -1,4 +1,23 @@
 package First_Lesson;
 
 public class Loops {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("If Statement");
+        if (a == 10){
+            System.out.println("Hello World");
+        }
+
+        int count = 0;
+        System.out.println("While Loop");
+        while(count != 5){
+            System.out.println(count);
+            count++;
+        }
+
+        System.out.println("For Loop");
+        for (int num = 1; num != 5; num++){
+            System.out.println(num);
+        }
+    }
 }
