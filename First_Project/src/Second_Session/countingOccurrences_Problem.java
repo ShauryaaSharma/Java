6b2af87ab6531e380d95d8c316963f3e07b080b9
Shaurya <shauryasofficial27@gmail.com>
2026-01-28 03:11:03 +0530

    Update First_Project/src/Second_Session/countingOccurrences_Problem.java

--- First_Project/src/Second_Session/countingOccurrences_Problem.java
+++ First_Project/src/Second_Session/countingOccurrences_Problem.java
@@ -1,4 +1,24 @@
 package Second_Session;
 
+import java.util.Scanner;
+
 public class countingOccurrences_Problem {
+    public static void main(String[] args) {
+        System.out.print("Provide a number: ");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        //There is one more way that we can convert the number to a string, and then we can iterate over the whole number. Try solving it on yourself.
+
+        int count = 0;
+        while (num > 0){
+            int rem = num % 10;
+            if (rem == 5){
+                count++;
+            }
+            num = num / 10;
+        }
+
+        System.out.println(count);
+    }
 }
