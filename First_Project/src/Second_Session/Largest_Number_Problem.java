0552ba6afdffba506ee88d52b9f36838f97af683
Shaurya <shauryasofficial27@gmail.com>
2026-01-28 03:11:02 +0530

    Update First_Project/src/Second_Session/Largest_Number_Problem.java

--- First_Project/src/Second_Session/Largest_Number_Problem.java
+++ First_Project/src/Second_Session/Largest_Number_Problem.java
@@ -1,4 +1,30 @@
 package Second_Session;
 
+import java.util.Scanner;
+
 public class Largest_Number_Problem {
+    public static void main(String[] args) {
+        System.out.println("Enter 3 Numeric Values: ");
+        Scanner inn = new Scanner(System.in);
+
+        int a = inn.nextInt();
+        int b = inn.nextInt();
+        int c = inn.nextInt();
+
+        int max = a;
+        if (b > a){
+            max = b;
+        }
+        if (c > b){
+            max = c;
+        }
+
+        System.out.println(max);
+
+
+        //OR
+
+        System.out.println(Math.max(34, 57));
+
+    }
 }
