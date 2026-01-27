d29980c44a2f5026c063d96452a699c734fca033
Shaurya <shauryasofficial27@gmail.com>
2026-01-28 03:11:03 +0530

    Update First_Project/src/Second_Session/conditionalStatements.java

--- First_Project/src/Second_Session/conditionalStatements.java
+++ First_Project/src/Second_Session/conditionalStatements.java
@@ -1,4 +1,16 @@
 package Second_Session;
 
 public class conditionalStatements {
+    public static void main(String[] args) {
+        int salary = 125000;
+        if (salary > 250000){
+            salary = salary + 5000;
+        } else if (salary < 50000) {
+            salary = salary + 1000;
+        } else {
+            salary = salary + 1000;
+        }
+
+        System.out.println(salary);
+    }
 }
