143c92350b0983e3486499b2b401b5f95153a138
Shaurya <shauryasofficial27@gmail.com>
2026-01-29 22:38:30 +0530

    Update First_Lesson/src/Main.java

--- First_Lesson/src/Main.java
+++ First_Lesson/src/Main.java
@@ -1,9 +1,7 @@
-package First_Lesson;
-
 // ~ Class is a named group of properties and functions.
 // ~ Public mean that the main class can be accessed from anywhere.
 // ~ Convention: Since a .java file is class itself. So the class inside the file should have the same name. Also Starting the class name from capital is a convention.
-// ~ First_Lesson.Main function is a block of code, and this is where the program starts. Without this main function we cannot run our code in Java.//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// ~ Main function is a block of code, and this is where the program starts. Without this main function we cannot run our code in Java.//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     // ~ Since the main function should be logically available to be executed from anywhere, hence the keyword "public" is used for main function.
